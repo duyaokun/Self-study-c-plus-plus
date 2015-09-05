82713c5025dcb78df997e3b5728dca4e575d39e1
杜耀坤 <duyaokun@foxmail.com>
2015-09-05 15:44:48 +0800

    format code

--- test1/cpp1.cpp
+++ test1/cpp1.cpp
@@ -6,7 +6,7 @@ using namespace std;
 #define NUMBER_SIZE 10 // 能输入整数的个数
 #endif
 
-int main(int argc, char const *argv[])
+int main(int argc, char const* argv[])
 {
   int* numbers = new int[NUMBER_SIZE];// 动态分配能存入10个int值的内存
   memset(numbers, 0, NUMBER_SIZE * sizeof(int));
@@ -14,7 +14,7 @@ int main(int argc, char const *argv[])
   cout << "请输入" << NUMBER_SIZE << "个整数:" << endl;
 
   // 从键盘输入数值
-  for (int i = 0; i < NUMBER_SIZE; ++i)
+  for(int i = 0; i < NUMBER_SIZE; ++i)
   {
     cout << "[" << i + 1 << "]";
     cin >> numbers[i];
@@ -23,7 +23,7 @@ int main(int argc, char const *argv[])
   // 计算和
   int sum = 0;
 
-  for (int i = 0; i < NUMBER_SIZE; ++i)
+  for(int i = 0; i < NUMBER_SIZE; ++i)
   {
     sum += numbers[i];
   }
@@ -33,9 +33,10 @@ int main(int argc, char const *argv[])
   // 计算最小值
   int minNumber = numbers[0];
 
-  for (int i = 1; i < NUMBER_SIZE; ++i)
+  for(int i = 1; i < NUMBER_SIZE; ++i)
   {
-    if (numbers[i] < minNumber) {
+    if(numbers[i] < minNumber)
+    {
       minNumber = numbers[i];
     }
   }
